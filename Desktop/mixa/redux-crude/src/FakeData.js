9855bab882018e059bb676c808223b839c20205c
Yacob Dita <akodita123@gmail.com>
2022-02-18 13:44:42 -0600

    redux files are added

---
+++ Desktop/mixa/redux-crude/src/FakeData.js
@@ -0,0 +1,42 @@
+export const UsersData =[
+    {
+        id:1,
+        name:"Leanne Graham",
+        usersname:"bret",
+    },
+    {
+        id:2,
+        name:"Ervin Howell",
+        usersname:"Antonette", 
+    },
+    {
+        id:3,
+        name:"Clementine bauch",
+        usersname:"clementine",
+    },
+    {
+        id:4,
+        name:"Patricia Lebsack",
+        usersname:"Karianne",
+    },
+    {
+       id:5,
+        name:"chelsey Dietrich",
+        usersname:"Kamren",
+    },
+    {
+        id:6,
+        name:"Jake Runolfsdottir V",
+        usersname:"Leopoldo_corkery",
+    },
+    {
+    id:7,
+    name:"Mrs.Dennis Schulist",
+    usersname:"Karley-Dach",
+    },
+    {
+      id:8,
+      name:"Kurtis Weissnat",
+      usersname:"Elwayn.Skiles",
+    }
+]
---
+++ Desktop/mixa/redux-crude/src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { configureStore} from '@reduxjs/toolkit';
+import { Provider } from "react-redux";
+import usersReducer from './features/Users'
+
+const store = configureStore({
+  reducer: {
+    users:usersReducer,
+  }
+})
+
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store ={store}>
+       <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
+reportWebVitals();
